08fecc43d4b125661df42ca8b238de902d8b8042
David Savage <david.savage.jr@gmail.com>
2013-11-20 23:22:38 -0600

    Initial stuff

---
+++ AutoParse.Tests/ScratchTests.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using NUnit.Framework;
+using Should;
+
+namespace AutoParse.Tests
+{
+    [TestFixture]
+    public class ScratchTests
+    {
+
+        [Test]
+        public void should_parse_value()
+        {
+            var val = "20131025".TryParse<DateTime>(DateTime.TryParse);
+            Console.WriteLine(val);
+        }
+    }
+}
---
+++ AutoParse/AutoParse.cs
@@ -0,0 +1,22 @@
+﻿using System;
+
+namespace AutoParse
+{
+    public static class AutoParse
+    {
+        public delegate bool TryParser<T>(string str, out T value);
+        public static T TryParse<T>(this string value, TryParser<T> parser)
+        {
+            T val;
+            parser.Invoke(value, out val);
+            return val;
+        }
+
+        public static T? TryParseNullable<T>(string value, TryParser<T> parser)
+            where T : struct
+        {
+            T val;
+            return parser.Invoke(value, out val) ? val : null as T?;
+        }
+    }
+}
